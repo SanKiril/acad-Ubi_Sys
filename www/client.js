cacf8ffddc8a07a7252f1f65a7390561ddf29c0e
SanKiril <santiago.kiril@gmail.com>
2024-04-05 00:25:11 +0200

    loads

--- www/client.js
+++ www/client.js
@@ -1,30 +1,131 @@
 const main_body = document.querySelector("main");
+let productsList;
+let favouritesList;
+let cartList;
 let products = [];
 
 const loadProducts = () => {
     // products list
-    const productsList = document.createElement("ul");
-    productsList.classList.add("products-list");
+    if (!productsList) {
+        productsList = document.createElement("ul");
+        productsList.classList.add("products-list");
+        
+        // add products to products list
+        products.forEach(product => {
+            // product item
+            const productItem = document.createElement("li");
+            productItem.classList.add("products-list-item");
+            productsList.appendChild(productItem);
+
+            // product item image
+            const productImage = document.createElement("img");
+            productImage.src = product.image;
+            productImage.alt = product.name;
+            productItem.appendChild(productImage);
+
+            // product item name
+            const productName = document.createElement("p");
+            productName.innerHTML = product.name;
+            productItem.appendChild(productName);
+        });
+
+        // load product info
+        productsList.addEventListener("pointerdown", event => {
+            const targetProduct = event.target.closest(".products-list-item");
+            if (targetProduct) {
+                const index = Array.from(productsList.children).indexOf(targetProduct);
+                const product = products[index];
+                loadProductInfo(product);
+            }
+        });
+    }
     main_body.appendChild(productsList);
+}
 
-    // add products to products list
-    products.forEach(product => {
-        // product item
-        const productItem = document.createElement("li");
-        productItem.classList.add("products-list-item");
-        productsList.appendChild(productItem);
+const loadFavourites = () => {
+    // clear main body
+    main_body.innerHTML = "";
 
-        // product item image
-        const productImage = document.createElement("img");
-        productImage.src = product.image;
-        productImage.alt = product.name;
-        productItem.appendChild(productImage);
+    // favourites list
+    if (!favouritesList) {
+        favouritesList = document.createElement("ul");
+        favouritesList.classList.add("products-list");
 
-        // product item name
-        const productName = document.createElement("p");
-        productName.innerHTML = product.name;
-        productItem.appendChild(productName);
-    });
+        // add products to favourites list
+        products.filter(product => product.favourite).forEach(product => {
+            // product item
+            const productItem = document.createElement("li");
+            productItem.classList.add("products-list-item");
+            favouritesList.appendChild(productItem);
+
+            // product item image
+            const productImage = document.createElement("img");
+            productImage.src = product.image;
+            productImage.alt = product.name;
+            productItem.appendChild(productImage);
+
+            // product item name
+            const productName = document.createElement("p");
+            productName.innerHTML = product.name;
+            productItem.appendChild(productName);
+        });
+
+        // load product info
+        productsList.addEventListener("pointerdown", event => {
+            const targetProduct = event.target.closest(".products-list-item");
+            if (targetProduct) {
+                const index = Array.from(productsList.children).indexOf(targetProduct);
+                const product = products[index];
+                loadProductInfo(product);
+            }
+        });
+    }
+    main_body.appendChild(favouritesList);
+}
+
+const loadCart = () => {
+    // clear main body
+    main_body.innerHTML = "";
+
+    // cart list
+    if (!cartList) {
+        cartList = document.createElement("ul");
+        cartList.classList.add("products-list");
+    
+        // add products to cart list
+        products.filter(product => product.cart).forEach(product => {
+            // product item
+            const productItem = document.createElement("li");
+            productItem.classList.add("products-list-item");
+            cartList.appendChild(productItem);
+
+            // product item image
+            const productImage = document.createElement("img");
+            productImage.src = product.image;
+            productImage.alt = product.name;
+            productItem.appendChild(productImage);
+
+            // product item name
+            const productName = document.createElement("p");
+            productName.innerHTML = product.name;
+            productItem.appendChild(productName);
+        });
+
+        // load product info
+        productsList.addEventListener("pointerdown", event => {
+            const targetProduct = event.target.closest(".products-list-item");
+            if (targetProduct) {
+                const index = Array.from(productsList.children).indexOf(targetProduct);
+                const product = products[index];
+                loadProductInfo(product);
+            }
+        });
+    }
+    main_body.appendChild(cartList);
+}
+
+const loadProductInfo = (product) => {
+    // TODO
 }
 
 const loadFooter = () => {
@@ -37,26 +138,32 @@ const loadFooter = () => {
     favourites.src = "icon-favourites.png";
     favourites.alt = "Favourites";
     footer.appendChild(favourites);
+    favourites.addEventListener("pointerdown", event => {
+        loadFavourites();
+    });  // load favourites
 
     // add cart
     const cart = document.createElement("img");
     cart.src = "icon-cart.png";
     cart.alt = "Cart";
     footer.appendChild(cart);
+    cart.addEventListener("pointerdown", event => {
+        loadCart();
+    });  // load cart
 }
 
-const loadMainMenu = async () => {
-    // SERVER-CLIENT
-    const response = await fetch("products.json");
+const loadMain = async () => {
+    // clear main body
+    main_body.innerHTML = "";
+    
+    // fetch products
+    if (products.length === 0) {
+        const response = await fetch("products.json");
+        products = await response.json();
+        loadFooter();
+    }
 
-    // CLIENT ONLY
-    products = await response.json();
     loadProducts();
-    loadFooter();
 }
 
-const loadProductInfo = () => {
-    // TODO
-}
-
-loadMainMenu();
+loadMain();
